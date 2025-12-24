a5b58a06630ee2650f0f402e86b18cf708d30031
Joseph Tiglao <joseph.tiglao@gmail.com>
2025-12-25 02:04:00 +0800

    run local and online

--- api/login.js
+++ api/login.js
@@ -18,6 +18,10 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
