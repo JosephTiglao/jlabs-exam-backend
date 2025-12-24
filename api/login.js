d0fedbb628d2da6ea566f4279eca8c0b97aa2c98
Joseph Tiglao <joseph.tiglao@gmail.com>
2025-12-25 02:04:17 +0800

    run local and online

--- api/login.js
+++ api/login.js
@@ -19,7 +19,7 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method == "OPTIONS") {
-    res.status(200).end();
+    return res.status(200).end();
   }
 
   if (req.method !== "POST") {
